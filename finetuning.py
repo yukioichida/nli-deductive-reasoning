ee4b8b7c1bd3d0e542b24e8981583a2cb7e67e85
Alexandre Yukio Ichida <alexandre.ichida@lojasrenner.com.br>
2021-05-18 09:19:44 -0300

    code format

--- finetuning.py
+++ finetuning.py
@@ -1,14 +1,15 @@
+import argparse
+import logging
+import math
+import ssl
+
 import torch
-from torch.utils.data import DataLoader
-from tqdm import tqdm
 from datasets import load_dataset, load_metric
+from tqdm import tqdm
 from transformers import AdamW, get_scheduler
 from transformers import XLNetForSequenceClassification, XLNetConfig, XLNetTokenizer
 
-import ssl
-import logging
-import math
-import argparse
+from src.nli_datasets import NLIDatasets
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -18,10 +19,7 @@ def validate(model, val_dataloader, metric):
     with torch.no_grad():
         for step, batch in enumerate(val_dataloader):
             if torch.cuda.is_available():
-                batch['attention_mask'] = batch['attention_mask'].to('cuda')
-                batch['input_ids'] = batch['input_ids'].to('cuda')
-                batch['token_type_ids'] = batch['token_type_ids'].to('cuda')
-                batch['label'] = batch['label'].to('cuda')
+                batch = {key: tensor.to('cuda') for key, tensor in batch.items()}
             outputs = model(attention_mask=batch['attention_mask'],
                             input_ids=batch['input_ids'],
                             token_type_ids=batch['token_type_ids'],
@@ -31,7 +29,9 @@ def validate(model, val_dataloader, metric):
         return metric.compute()
 
 
-def get_optimizers(model: torch.nn.Module, lr: float, max_train_steps: float):
+def get_optimizers(model: torch.nn.Module, lr: float, train_cycles: int, gradient_accumulation_steps: int):
+    num_update_steps_per_epoch = math.ceil(train_cycles / gradient_accumulation_steps)
+    max_train_steps = args.epochs * num_update_steps_per_epoch
     # Optimizer -  Split weights in two groups, one with weight decay and the other not.
     no_decay = ["bias", "LayerNorm.weight"]
     optimizer_grouped_parameters = [
@@ -54,34 +54,14 @@ def get_optimizers(model: torch.nn.Module, lr: float, max_train_steps: float):
     return optimizer, lr_scheduler
 
 
-def get_dataloaders(train_batch_size: int, val_batch_size: int, tokenizer: XLNetTokenizer, tokenizer_workers: int = 20):
-    mnli_dataset = load_dataset('glue', 'mnli')
-    print(mnli_dataset.column_names)
-    dataset = mnli_dataset.map(
-        lambda row: tokenizer(row['premise'], row['hypothesis'], truncation=True, padding='max_length',
-                              max_length=128), batched=True, num_proc=tokenizer_workers)
-    dataset.set_format(type='torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'label'])
-    train_dataloader = DataLoader(dataset['train'], batch_size=train_batch_size)
-    val_matched_dataloader = DataLoader(dataset['validation_matched'], batch_size=val_batch_size)
-    return train_dataloader, val_matched_dataloader
-
-
 def get_logger():
     # set up logging to file
-    format = '[%(asctime)s]- %(message)s'
-    logging.basicConfig(
-        filename='train_details.log',
-        level=logging.INFO,
-        format=format,
-        datefmt='%H:%M:%S'
-    )
-    # set up logging to console
+    log_format = '%(asctime)s - %(message)s'
+    logging.basicConfig(filename='train_details.log', level=logging.INFO, format=log_format, datefmt='%H:%M:%S')
     console = logging.StreamHandler()
-    console.setLevel(logging.DEBUG)
-    # set a format which is simpler for console use
-    formatter = logging.Formatter(format)
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter(log_format)
     console.setFormatter(formatter)
-    # add the handler to the root logger
     logging.getLogger().addHandler(console)
     logger = logging.getLogger(__name__)
     return logger
@@ -89,7 +69,7 @@ def get_logger():
 
 def train(lr: float, train_batch_size: int, val_batch_size: int, gradient_accumulation_steps: int, val_step: int,
           epochs=int):
-    model_name = 'xlnet-base-cased'
+    model_name = "xlnet-base-cased"
     config = XLNetConfig.from_pretrained(model_name, num_labels=3)
     tokenizer = XLNetTokenizer.from_pretrained(model_name, config=config)
     model = XLNetForSequenceClassification.from_pretrained(model_name, config=config)
@@ -97,26 +77,23 @@ def train(lr: float, train_batch_size: int, val_batch_size: int, gradient_accumu
         model = model.to('cuda')
     
     log = get_logger()
+    log.info('Loading dataset...')
+    nli_dataset = NLIDatasets(tokenizer=tokenizer)
+    train_loader = nli_dataset.get_train_dataloader(train_batch_size=train_batch_size)
+    val_m_loader, val_mm_loader = nli_dataset.get_mnli_dev_dataloaders(val_batch_size=val_batch_size)
     
-    train_dataloader, val_dataloader = get_dataloaders(train_batch_size=train_batch_size,
-                                                       val_batch_size=val_batch_size,
-                                                       tokenizer=tokenizer)
-    train_loader_len = len(train_dataloader)
-    num_update_steps_per_epoch = math.ceil(train_loader_len / gradient_accumulation_steps)
-    max_train_steps = args.epochs * num_update_steps_per_epoch
     metric = load_metric("accuracy")
-    optimizer, lr_scheduler = get_optimizers(model=model, lr=lr, max_train_steps=max_train_steps)
+    train_loader_len = len(train_loader)
+    optimizer, lr_scheduler = get_optimizers(model=model, lr=lr, train_cycles=train_loader_len,
+                                             gradient_accumulation_steps=gradient_accumulation_steps)
     
     # Train
     log.info("Train...")
     for epoch in range(epochs):
-        for step, batch in tqdm(enumerate(train_dataloader), total=train_loader_len):
+        for step, batch in tqdm(enumerate(train_loader), total=train_loader_len):
             model.train()
             if torch.cuda.is_available():
-                batch['attention_mask'] = batch['attention_mask'].to('cuda')
-                batch['input_ids'] = batch['input_ids'].to('cuda')
-                batch['token_type_ids'] = batch['token_type_ids'].to('cuda')
-                batch['label'] = batch['label'].to('cuda')
+                batch = {key: tensor.to('cuda') for key, tensor in batch.items()}
             
             outputs = model(attention_mask=batch['attention_mask'],
                             input_ids=batch['input_ids'],
@@ -124,13 +101,13 @@ def train(lr: float, train_batch_size: int, val_batch_size: int, gradient_accumu
                             labels=batch['label'])
             loss = outputs.loss / gradient_accumulation_steps
             loss.backward()
-            if (step + 1) % gradient_accumulation_steps == 0 or step == len(train_dataloader) - 1:
+            if (step + 1) % gradient_accumulation_steps == 0 or step == train_loader_len - 1:
                 # Propagation
                 optimizer.step()
                 lr_scheduler.step()
                 optimizer.zero_grad()
             if (step + 1) % val_step == 0:
-                val_acc = validate(model, val_dataloader, metric)
+                val_acc = validate(model, val_m_loader, metric)
                 log.info(f"{epoch} - {step} - Val acc matched: {val_acc:.4f}")
 
 
---
+++ src/nli_datasets.py
@@ -0,0 +1,47 @@
+from datasets import load_dataset, load_metric, concatenate_datasets, Metric, Dataset
+from transformers import PreTrainedTokenizer
+from torch.utils.data import DataLoader
+
+
+class NLIDatasets:
+    
+    def __init__(self, tokenizer: PreTrainedTokenizer, max_length: int = 128, thread: int = 4):
+        self.metric = load_metric('accuracy')
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.mnli_dataset = self._load_nli_datasets(load_dataset('glue', 'mnli'), thread=thread).remove_columns('idx')
+        self.snli_dataset = self._load_nli_datasets(load_dataset('snli'), thread=thread)
+    
+    def get_metric(self) -> Metric:
+        return self.metric
+    
+    def _load_nli_datasets(self, dataset: Dataset, thread: int = 4) -> Dataset:
+        loaded_dataset = dataset.map(
+            lambda row: self.tokenizer(row['premise'],
+                                       row['hypothesis'],
+                                       truncation=True,
+                                       padding='max_length',
+                                       max_length=self.max_length),
+            batched=True,
+            num_proc=thread)
+        loaded_dataset.set_format(type='torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'label'])
+        return loaded_dataset
+    
+    def get_train_dataloader(self, train_batch_size: int) -> DataLoader:
+        train_mnli_dataset = self.mnli_dataset['train']
+        train_snli_dataset = self.snli_dataset['train']
+        print(f"MNLI Train Set length: {len(train_mnli_dataset)}")
+        print(f"SNLI Train Set length: {len(train_snli_dataset)}")
+        train_dataset = concatenate_datasets([train_mnli_dataset, train_snli_dataset])
+        print(f"Total train length {len(train_dataset)}")
+        return DataLoader(train_dataset, batch_size=train_batch_size)
+    
+    def get_mnli_dev_dataloaders(self, val_batch_size: int) -> (DataLoader, DataLoader):
+        val_matched = self.mnli_dataset['validation_matched']
+        val_mismatched = self.mnli_dataset['validation_mismatched']
+        return DataLoader(val_matched, batch_size=val_batch_size), DataLoader(val_mismatched, batch_size=val_batch_size)
+    
+    def get_snli_dev_test_dataloaders(self, val_batch_size: int) -> (DataLoader, DataLoader):
+        snli_val = self.snli_dataset['validation']
+        sni_test = self.snli_dataset['test']
+        return DataLoader(snli_val, batch_size=val_batch_size), DataLoader(sni_test, batch_size=val_batch_size)
